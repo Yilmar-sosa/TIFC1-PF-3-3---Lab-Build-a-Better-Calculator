afa87d29aa4cff5343b37a3708e5fdfb4d72e8eb
Yilmar-sosa <yilmarsosa70@gmail.com>
2026-04-24 15:24:55 -0500

    Mi tercer laboratorio

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
-def main():
-  print("Hello learners!")
 
-if __name__=="__main__":
-  main()
+
+
+
+def addmultiplenumbers(numbers):
+    total = 0
+    for num in numbers:
+       total += num
+    return total
+
+def multiplymultiplenumbers(numbers):
+    total = 0 
+    for num in numbers:
+       total *= num
+    return total
+
+def isiteven(num): 
+    if isinstance(num, int) and num % 2 == 0:
+        return True
+    else:
+        return False
+
+def isitaninteger(num):
+    if isinstance(num, int):
+        return True
+    else:
+        return False
+def main():
+    print("Calculadora mejorada")
+    nums = [5,8,10,13]
+
+    print("suma", addmultiplenumbers(nums))
+    print("multiplicacion", multiplymultiplenumbers(nums))
+    print("¿Es par?", isinstance(3))
+    print("¿Es entero?", isinstance(5))
+
+    if __name__=="__main__":
+      main()
